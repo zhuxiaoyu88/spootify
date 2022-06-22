5418c43717ce745a6278ef1a0809c17b1ed63df8
Anita <46235179+zhuxiaoyu88@users.noreply.github.com>
2022-06-22 15:48:19 +0800

    implement API

--- src/routes/Discover/components/Discover.js
+++ src/routes/Discover/components/Discover.js
@@ -1,6 +1,9 @@
-import React, { Component } from 'react';
-import DiscoverBlock from './DiscoverBlock/components/DiscoverBlock';
-import '../styles/_discover.scss';
+import React, { Component } from "react";
+import DiscoverBlock from "./DiscoverBlock/components/DiscoverBlock";
+import "../styles/_discover.scss";
+import axios from "axios";
+
+const MAIN_ENDPOINT = "https://api.spotify.com/v1/browse";
 
 export default class Discover extends Component {
   constructor() {
@@ -9,8 +12,58 @@ export default class Discover extends Component {
     this.state = {
       newReleases: [],
       playlists: [],
-      categories: []
+      categories: [],
+      token: ""
     };
+  }
+
+  async componentDidMount() {
+    axios("https://accounts.spotify.com/api/token", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        Authorization:
+          "Basic " +
+          new Buffer(
+            REACT_APP_SPOTIFY_CLIENT_ID + ":" + REACT_APP_SPOTIFY_CLIENT_SECRET
+          ).toString("base64")
+      },
+      data: "grant_type=client_credentials"
+    })
+      .then((tokenResponse) => {
+        this.setState({ token: tokenResponse.data.access_token });
+
+        const axiosInstanceSpotify = axios.create({
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+            Authorization: "Bearer " + this.state.token
+          }
+        });
+
+        axios
+          .all([
+            axiosInstanceSpotify
+              .get(`${MAIN_ENDPOINT}/new-releases`)
+              .catch((error) => console.log(error)),
+            axiosInstanceSpotify
+              .get(`${MAIN_ENDPOINT}/featured-playlists`)
+              .catch((error) => console.log(error)),
+            axiosInstanceSpotify
+              .get(`${MAIN_ENDPOINT}/categories`)
+              .catch((error) => console.log(error))
+          ])
+          .then(
+            axios.spread((resReleases, resPlaylists, resCategories) => {
+              this.setState({ newReleases: resReleases.data.albums.items });
+              this.setState({ playlists: resPlaylists.data.playlists.items });
+              this.setState({
+                categories: resCategories.data.categories.items
+              });
+            })
+          );
+      })
+      .catch((error) => console.log(error));
   }
 
   render() {
@@ -18,9 +71,22 @@ export default class Discover extends Component {
 
     return (
       <div className="discover">
-        <DiscoverBlock text="RELEASED THIS WEEK" id="released" data={newReleases} />
-        <DiscoverBlock text="FEATURED PLAYLISTS" id="featured" data={playlists} />
-        <DiscoverBlock text="BROWSE" id="browse" data={categories} imagesKey="icons" />
+        <DiscoverBlock
+          text="RELEASED THIS WEEK"
+          id="released"
+          data={newReleases}
+        />
+        <DiscoverBlock
+          text="FEATURED PLAYLISTS"
+          id="featured"
+          data={playlists}
+        />
+        <DiscoverBlock
+          text="BROWSE"
+          id="browse"
+          data={categories}
+          imagesKey="icons"
+        />
       </div>
     );
   }
